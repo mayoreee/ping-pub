b5432caf32c0525c0b3bcc235e4a5ece436c5995
mayoreee <mayoreee1@gmail.com>
2023-10-07 12:53:32 +0200

    feat: jmes branding

--- src/layouts/components/DefaultLayout.vue
+++ src/layouts/components/DefaultLayout.vue
@@ -61,9 +61,9 @@ function selected(route: any, nav: NavLink) {
     >
       <div class="flex justify-between mt-1 pl-4 py-4 mb-1">
         <RouterLink to="/" class="flex items-center">
-          <img class="w-10 h-10" src="../../assets/logo.svg" />
+
           <h1 class="flex-1 ml-3 text-2xl font-semibold dark:text-white">
-            Ping.pub
+            JMES
           </h1>
         </RouterLink>
         <div
